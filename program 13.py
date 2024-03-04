6911a21f97be96f9d1da4214e9b417a4fab206a4
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-03-05 00:07:30 +0530

    Add files via upload

---
+++ program 13.py
@@ -0,0 +1,7 @@
+#Wap to print all the names from list whose length greater than 6.
+
+names = ["John", "Alice", "Michael", "Elizabeth", "Christopher", "Jennifer", "Jonathan"]
+
+for name in names:
+    if len(name) > 6:
+        print(name)
