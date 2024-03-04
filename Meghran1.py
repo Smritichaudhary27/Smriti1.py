388363aca557d3545f248536d1ae35f524e393ec
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-03-04 18:16:38 +0530

    Delete Meghran1.py

--- Meghran1.py
+++
@@ -1,9 +0,0 @@
-# Write a program to determine the type of input using match statement.
-data= int(input("enter value:"))
-match data:
-case1:
-print(1)
-case2:
-print(2)
-case_:
-print("Wrong number")
