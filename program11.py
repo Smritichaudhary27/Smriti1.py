cc824d08e817e8623fad8e1efe050ffb90f5b8be
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-03-04 22:11:32 +0530

    Add files via upload

---
+++ program11.py
@@ -0,0 +1,16 @@
+#Write a program to generate fibonnaci sequence upto n terms using while loop. 
+
+n= int(input("Enter the value of nth term: ")) 
+a = 0 
+b = 1 
+c = 0 
+count = 1 
+print("Fibonacci Series: ", end = " ") 
+while(count <= n):
+    print(c, end = " ") 
+    count += 1 
+    a = b 
+    b = sum 
+    sum = a + b
+
+
